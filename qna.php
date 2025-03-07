526512a0f3e51326faed7390adddc968f919af73
OliverKunsky <oliver.kunsky@student.ukf.sk>
2025-03-07 11:46:16 +0100

    otazky php

--- qna.php
+++ qna.php
@@ -37,6 +37,15 @@
       <div class="row">
         <div class="col-100 text-center">
           <p><strong><em>Elit culpa id mollit irure sit. Ex ut et ea esse culpa officia ea incididunt elit velit veniam qui. Mollit deserunt culpa incididunt laborum commodo in culpa.</em></strong></p>
+          <section class="container">
+            <?php include "otazky.php"; ?>
+            <?php for ($i = 0; $i < count($otazky); $i++) { ?>
+                <div class="accordion">
+                    <div class="question"><?php echo $otazky[$i]; ?></div>
+                    <div class="answer"><?php echo $odpovede[$i]; ?></div>
+                </div>
+            <?php } ?>
+        </section>
         </div>
       </div>
     </section>
